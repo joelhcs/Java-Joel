d78d7f78c761c9a4639e8edee9d36ed732543b93
Joel Hardt Candido da Silva <jhcsilva@furb.br>
2026-03-10 21:40:34 -0300

    novos exercícios

---
+++ src/exercícios/Ex17.java
@@ -0,0 +1,17 @@
+package exercícios;
+import java.util.Scanner;
+
+public class Ex17 {
+public static void main(String[] args) {
+    
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("Quantas camisetas o cliente comprou?");
+    Double camisetas = teclado.nextDouble();
+    Double valorTotal = camisetas * 35;
+    Double desconto = (valorTotal/10) ;
+    Double valorFinal = valorTotal - desconto;
+    System.out.println("Valor total: R$" + valorTotal + "\nDesconto aplicado: R$" + desconto + "\nValor final: R$" + valorFinal);
+
+}
+}
