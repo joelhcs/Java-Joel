60dd962a6a40e33d69a54fce65872953d3041b35
Joel Hardt Candido da Silva <jhcsilva@furb.br>
2026-03-11 20:54:49 -0300

    tarefa

---
+++ src/exercícios/Ex20.java
@@ -0,0 +1,17 @@
+package exercícios;
+
+import java.util.Scanner;
+
+public class Ex20 {
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Quantos litros de gasolina foram utilizados?");
+    int gasolina = scan.nextInt();
+    System.out.println("Quantos litros de etanol foram utilizados?");
+    int etanol = scan.nextInt();
+    Double precoGasolina = gasolina*5.75;
+    Double precoEtanol = etanol*4.29;
+    Double valorTotal = precoGasolina + precoEtanol;
+    System.out.println("Valor gasolina: R$" + precoGasolina + "\nValor etanol: R$" + precoEtanol + "\nValor total: R$" + valorTotal);
+}
+}
