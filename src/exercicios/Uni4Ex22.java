1929ac9947373b3b5141b40c4dd2d9efa95aca17
Joel Hardt Candido da Silva <jhcsilva@furb.br>
2026-03-26 20:03:41 -0300

    Exercício switch

---
+++ src/exercicios/Uni4Ex22.java
@@ -0,0 +1,22 @@
+package exercicios;
+import java.util.Scanner;
+public class Uni4Ex22 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite o número do seu curso:");
+    System.out.println("1 - Ciência da Computação\n2 - Licenciatura da computação\n3 - Sistemas de Informação");
+    int curso = sc.nextInt();
+    switch (curso) {
+        case 1:
+            System.out.println("Bacharel em Ciência da Computação");
+            break;
+        case 2:
+            System.out.println("Licenciado em Computação");
+            break;
+        case 3:
+            System.out.println("Bacharel em Sistemas de Informação");
+            default:
+            break;
+    }
+}
+}
