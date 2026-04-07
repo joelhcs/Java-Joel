4904635cf5bc9b768e248897588f696fad26951d
Joel Hardt Candido da Silva <jhcsilva@furb.br>
2026-04-07 20:34:56 -0300

    Exercício beecrowd

---
+++ src/exercicios/Uni4Uri1042.java
@@ -0,0 +1,34 @@
+package exercicios;
+import java.util.Scanner;
+public class Uni4Uri1042 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int valorMaior, valorMeio, valorMenor;
+    if (a > b && a > c) {
+        valorMaior = a;
+    } else if (b > a && b > c) {
+        valorMaior = b;
+    } else {
+        valorMaior = c;
+    }
+    if ((a > b && a < c) || (a < b && a > c)) {
+        valorMeio = a;
+    } else if ((b > a && b < c) || (b < a && b > c)) {
+        valorMeio = b;
+    } else {
+        valorMeio = c;
+    }
+    if (a < b && a < c) {
+        valorMenor = a;
+    } else if (b < a && b < c) {
+        valorMenor = b;
+    } else {
+        valorMenor = c;
+    }
+    System.out.println(valorMenor + "\n" + valorMeio + "\n" + valorMaior + "\n");
+    System.out.println(a + "\n" + b + "\n" + c);
+}
+}
